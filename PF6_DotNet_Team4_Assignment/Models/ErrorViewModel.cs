cf4885eb2c0e9760707bd6f6030d217237673c99
Alkiviadis Triantaf <triasalkis@yahoo.com>
2021-05-24 20:54:48 +0300

    Create Final Project Version 1.0

---
+++ PF6_DotNet_Team4_Assignment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PF6_DotNet_Team4_Assignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
