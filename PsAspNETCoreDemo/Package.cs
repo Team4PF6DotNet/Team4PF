096bb4444a27692603cabd13e68c3775843db6c8
markarapanou <77274987+markarapanou@users.noreply.github.com>
2021-05-27 17:12:46 +0300

    Add files via upload

---
+++ PsAspNETCoreDemo/Package.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PsAspNETCoreDemo.Models
+{
+    public class Package
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Reward { get; set; }
+        public decimal Fund_ammount { get; set; }
+        //public List<FundingPackages> FundingPackages { get; set; }
+    }
+}
---
+++ PsAspNETCoreDemo/Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PsAspNETCoreDemo.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Tittle { get; set; }
+        public string Description { get; set; } 
+        public string Category { get; set; }
+        public decimal FinancialProgress { get; set; }
+        //public List<AllProjects> AllProjects { get; set; }
+    }
+}
